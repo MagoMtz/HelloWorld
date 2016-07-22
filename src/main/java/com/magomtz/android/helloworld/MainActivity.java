6d640f6edaf46b4cff899a19cdfb6adb1211e279
MagoMtz <magomtz94@gmail.com>
2016-07-22 00:06:16 -0500

    Update MainActivity.java
    agregé el código para modificar el textView. Falta importar su clase

--- src/main/java/com/magomtz/android/helloworld/MainActivity.java
+++ src/main/java/com/magomtz/android/helloworld/MainActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    //@Bind(R.id.textView)
+    //TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView.setText("Texto nuevo");
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://jorgemtzitc.com/cv"));
         startActivity(intent);
     }
